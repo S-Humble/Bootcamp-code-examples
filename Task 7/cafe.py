0220642f0e4c17602e088868340c412090915b12
S-Humble <sam.m.humble@gmail.com>
2024-03-30 16:00:43 +0000

    Menu and stick created

---
+++ Task 7/cafe.py
@@ -0,0 +1,11 @@
+menu = [("cake", 8), ("coffee", 5), ("sandwich", 6), ("brownie", 8), ("tea", 30)]
+
+stock = dict(menu)
+print (stock)
+
+
+
+# make a list called menu
+# create a dictionary called 'stock'. Stock will contain a value for each item
+# make a dict called price. will contain the value for each item in the menu
+# calculate total_stock
