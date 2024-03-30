ad965d66c68d7aa63dbab362efba9a7814f562b3
S-Humble <sam.m.humble@gmail.com>
2024-03-30 17:41:17 +0000

    Individual values calculated

--- Task 7/cafe.py
+++ Task 7/cafe.py
@@ -1,11 +1,25 @@
-menu = [("cake", 8), ("coffee", 5), ("sandwich", 6), ("brownie", 8), ("tea", 30)]
+menu = ['apple', 'cake', 'brownie', 'coffee', 'tea']
 
-stock = dict(menu)
-print (stock)
+stock = {'apple' : 5, 
+         'cake' : 5, 
+         'brownie' : 8, 
+         'coffee' : 2, 
+         'tea' : 10}
+
+price = {'apple' : 2, 
+         'cake' : 5, 
+         'brownie' : 4.5,
+         'coffee' : 2, 
+         'tea' : 1.5}
+
+total_stock = {item: stock[item] * price[item] for item in stock if item in menu}
+#creates new dictionary containing keys based on menu and multiplication of the values from price and stock
+#uses items() method
+#if item removed from menu, stock controll doesn't represent it even if it is in either dictionary
+apple_value = (stock['apple'] * price['apple'])
+# I need to multiply the value from stock with the value from price
+#total_stock = (stock.values() * price.values())
 
 
 
-# make a list called menu
-# create a dictionary called 'stock'. Stock will contain a value for each item
-# make a dict called price. will contain the value for each item in the menu
-# calculate total_stock
+print (f"The value of {menu} is {total_stock}")
