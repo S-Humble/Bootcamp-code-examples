6241388d3d58e11af6a99d8b0ffac7a5e6bf219c
S-Humble <sam.m.humble@gmail.com>
2024-04-02 16:20:11 +0100

    First attempt

---
+++ Task 8/student_register.py
@@ -0,0 +1,10 @@
+with open ('reg_form.txt', 'w') as file: 
+
+    number_students = int(input("How many students are entering?"))
+
+    for i in range (number_students):
+        student_id = int(input("Enter the student ID:"))
+
+#write the student ID into a documenft called reg_form.txt
+#Include a dotted line after each student ID to act as a line to sign on 
+          
