caa3a7da523e29eede3452eaf8efcee70926f016
S-Humble <sam.m.humble@gmail.com>
2024-04-02 17:07:51 +0100

    Writes student ID with new prefix

--- Task 8/student_register.py
+++ Task 8/student_register.py
@@ -1,9 +1,15 @@
+number_students = int(input("How many students are entering?"))
+
 with open ('reg_form.txt', 'w') as file: 
 
-    number_students = int(input("How many students are entering?"))
-
     for i in range (number_students):
-        student_id = int(input("Enter the student ID:"))
+        student_id_num= int(input("Enter student ID number: "))
+        file.write("Student ID:"+  str(student_id_num) + "\n")
+
+
+       # student_id = ("Student ID:", student_id_num, "\n")
+        #file.write("Student ID:" + '\n')
+        #file.write(str(student_id_num))#+ "\n")
 
 #write the student ID into a documenft called reg_form.txt
 #Include a dotted line after each student ID to act as a line to sign on 
